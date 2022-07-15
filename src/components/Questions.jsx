ba2472ddc68a2151fc6a25b931e1424faa5d73de
kiburtaite <kiburtaite@gmail.com>
2022-07-15 04:32:33 +0300

    sort by date

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -4,6 +4,7 @@ import Question0 from './Question0';
 const Questions = () => {
 
   const [questions, setQuestions] = useState([]);
+  const [order, setOrder] = useState(0);
 
   useEffect(() => {
     fetch('http://localhost:5000/api/questions')
@@ -11,11 +12,23 @@ const Questions = () => {
       .then(data => setQuestions(data)))
   }, []);
 
-    return (
+  const dateAsc = (a, b) => a.date > b.date ? 1 : -1;
+  const dateDesc = (a, b) => a.date > b.date ? -1 : 1;
+
+  return (
+    <div>
+      <div>
+        <span>Rikiavimas </span>
+        {order ?
+          <button onClick={() => setOrder(0)}>Pagal datą</button> :
+          <button onClick={() => setOrder(1)}>Pagal datą</button>
+        }
+      </div>
       <div>
         {
           questions ? 
-          questions.map(question => 
+          questions.sort(order ? dateDesc : dateAsc)
+          .map(question => 
             <Question0
             id={question.id}
             title={question.title}
@@ -25,7 +38,8 @@ const Questions = () => {
           : <p>Loading...</p>
         }
       </div>
-    );
-  }
+    </div>
+  );
+}
   
-  export default Questions
+export default Questions
