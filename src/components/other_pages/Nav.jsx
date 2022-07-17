a591cb0ffa7e4e18195ea2631760928ca47bb0a3
kiburtaite <kiburtaite@gmail.com>
2022-07-17 07:29:18 +0300

    logout

--- src/components/other_pages/Nav.jsx
+++ src/components/other_pages/Nav.jsx
@@ -1,10 +1,16 @@
 import { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Authorized } from '../../App';
 
 const Nav = () => {
 
     const authorized = useContext(Authorized);
+    const navigate = useNavigate();
+
+    const logout = () => {
+        localStorage.clear();
+        navigate('/')
+    }; 
 
     return (
       <div>
@@ -12,6 +18,7 @@ const Nav = () => {
         {authorized ? <Link to='/ask'>Užduoti naują klausimą</Link> : null}
         {authorized ? null : <Link to='/login'>Prisijungti</Link>}
         {authorized ? null : <Link to='/register'>Registruotis</Link>}
+        {authorized ? <button onClick={logout}>Atsijungti</button>: null}
       </div>
     );
   }
