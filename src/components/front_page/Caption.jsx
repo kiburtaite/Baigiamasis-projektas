084f8c83ac923e243a09fcdf4e253779e774ae0a
kiburtaite <kiburtaite@gmail.com>
2022-07-17 10:48:13 +0300

    lt numbers

--- src/components/front_page/Caption.jsx
+++ src/components/front_page/Caption.jsx
@@ -1,12 +1,18 @@
 import { Link } from 'react-router-dom';
 
 const Caption = ( {id, title, user, date, answers} ) => {
+
+  const lastDigit = String(answers).slice(-1);
+
   return (
     <div key={id}>
       <Link to={`/question/${id}`}><h3>{title}</h3></Link>
       <h4>{user}</h4>
       <h4>{date}</h4>
-      <p>{answers} atsakymai</p>
+      <p>{answers}{
+        answers >= 10 && answers <= 19 || lastDigit == 0 ? " atsakymų"
+         : lastDigit == 1 ? " atsakymas" : " atsakymai"
+      }</p>
     </div>
   );
 }
